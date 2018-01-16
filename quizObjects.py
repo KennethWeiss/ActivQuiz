135bf9b046ac3b332eeddb71a1e34a25e839cdc2
KennethWeiss <kenny1weiss@aol.com>
2018-01-16 05:09:21 +0000

    Added 14 functions 7 need work

--- quizObjects.py
+++ quizObjects.py
@@ -1,4 +1,9 @@
-#Maybe I should convert runSheetMaker into a method of the object sheet this could be used to initialize the sheet
+#I'm working on the runSheetMaker methods - Kenny
+#I have objects working for
+#creating a sheet
+#insert numeric
+#insert multiple
+#THE CHECKTYPE SEEMS A BIT SLOPPY BUT IT WORKS :(
 #SOMETHING IS WRONG WITH GETRANDNOTZERO
 #!/usr/bin/python2.7
 # -*- coding: cp1252 -*-
@@ -20,7 +25,7 @@ class Question:
     level=0
     numberOfQuestions=10
     multipleChoices = {}
-
+    num = 0
 
 class Sheet:
     row = 0
@@ -29,182 +34,270 @@ class Sheet:
     quiz = "Quiz"
     fileName = "Filename"
 
-testSheet = Sheet()
-testSheet.fileName = "convertLinear"
-testSheet.title = "Title"
-testSheet.quiz = "Quiz"
+    w = Workbook()
+    ws = w.add_sheet('OK', cell_overwrite_ok=True)
+
+#create a Spreadsheet with Title and Quiz
+    def initialSheet():
+       #Sheet.fileName = functions
+       print("Creating initial template")
+       Sheet.row = 4
+       Sheet.column = 1
+       Question.level = 1
+       Question.answerType = 'Numeric'
+       Sheet.ws.write(1,1,'Title')
+       Sheet.ws.write(2,1,'Quiz')
+
+#insert a question and numerical answer to the spreadsheet
+    def insertNumeric(function):
+        print("adding numeric question")
+        Sheet.ws.write(Sheet.row,Sheet.column, 'Q' + str(Question.num))
+        Question.num+=1
+        Sheet.ws.write(Sheet.row,Sheet.column+1,Question.question)
+        Sheet.row+=1
+        Sheet.ws.write(Sheet.row,Sheet.column,"Level")
+        Sheet.ws.write(Sheet.row,Sheet.column+1,Question.level)
+        Sheet.row+=1
+        Sheet.ws.write(Sheet.row,Sheet.column,"Question Type")
+        Sheet.ws.write(Sheet.row,Sheet.column+1,Question.answerType)
+        Sheet.row+=1
+        Sheet.ws.write(Sheet.row,Sheet.column,"Correct Answers")
+        #write an answer to a cell from function
+        Sheet.ws.write(Sheet.row,Sheet.column+1,Question.answer)
+        Sheet.row+=3;
+
+#insert a multiple choice question and answer into the spreadsheet
+    def insertMultipleChoice(function):
+        Sheet.ws.write(Sheet.row,Sheet.column, 'Q' + str(Question.num))
+        Question.num += 1
+        #write a question from function
+        Sheet.ws.write(Sheet.row,Sheet.column+1,Question.question)
+        Sheet.row+=1
+        Sheet.ws.write(Sheet.row,Sheet.column,"Level")
+        Sheet.ws.write(Sheet.row,Sheet.column+1,Question.level)
+        Sheet.row+=1
+        Sheet.ws.write(Sheet.row,Sheet.column,"Question Type")
+        Question.answerType = "Multiple Choice"
+        Sheet.ws.write(Sheet.row,Sheet.column+1,Question.answerType)
+        Sheet.row+=1
+        Sheet.ws.write(Sheet.row,Sheet.column,"Correct Answers")
+        Sheet.ws.write(Sheet.row,Sheet.column+1,Question.answer)
+        #write an answer to a cell from function
+        for letter, choice in Question.multipleChoices.items():
+            Sheet.row+=1
+            Sheet.ws.write(Sheet.row,Sheet.column,letter)
+            Sheet.ws.write(Sheet.row,Sheet.column+1,choice)
+            print(letter, choice)
+        Sheet.row+=3
+
+#insert any function that has a question and answer
+#send question and answer to proper sheetInsert
+    def insertProblem(function):
+        function()
+        if(Question.answerType == "numeric"):
+            Sheet.insertNumeric(function)
+        elif(Question.answerType == "multipleChoice"):
+            Sheet.insertMultipleChoice(function)
+        else:
+            print("Problem with type")
+
+
 
 #def getRandNotZero(first,last):
 #   numbers = range(first,-1) + range(1,last)
 #   return random.choice(numbers)
 
+#def add():
+#def addMultipleChoice():
+#def subtract():
+#def addSubtract():
+#def multiply():
+#def divide():
+#def multiplyDivide():
+#def allOperations():
+#def divideSingleDigitQuotient():
+#def divideSingleDigitQuotientMultiple10():
+#def divideDoubleDigitQuotient():
+#def divideDoubleDigitQuotientMultiple10():
+#def divideDoubleDigitDivisor():
+#def divideDecimalEasy():
+#def divideDecimalMedium():
+#def divideDecimalHard():
 
 
 def add():
+    Question.answerType = "numeric"
     firstInt = random.randint(1,12)
     secondInt = random.randint(1,12)
     Question.question = (str(firstInt)+" + "+str(secondInt))
     Question.answer = firstInt+secondInt
     print("The question is " + Question.question)
-    Question.answerType = "Numeric"
+
 
 def addMultipleChoice():
+    Question.answerType = "multipleChoice"
     firstInt = random.randint(1,12)
     secondInt = random.randint(1,12)
     Question.question = (str(firstInt)+" + "+str(secondInt))
     Question.answer = firstInt+secondInt
     print("The question is " + Question.question)
-    Question.answerType = "MultipleChoice"
     Question.multipleChoices["A"] = firstInt - secondInt
     Question.multipleChoices["B"] = firstInt + secondInt
     Question.multipleChoices["C"] = firstInt * secondInt
     Question.multipleChoices["D"] = firstInt + firstInt
 
 def subtract():
-   firstInt = getRandNotZero(1,30);
-   secondInt = getRandNotZero(1,30);
-   if(secondInt>firstInt):
+    Question.type = "numerical"
+    firstInt = random.randint(1,30);
+    secondInt = random.randint(1,30);
+    if(secondInt>firstInt):
       Question.question, Question.answer = (str(secondInt)+" - "+str(firstInt),secondInt-firstInt)
-   else:
+    else:
       Question.question, Question.answer = (str(firstInt)+" - "+str(secondInt),firstInt-secondInt)
+
+def addSubtract():
+    Question.type = "numerical"
+    choice = random.choice('as')
+    if(choice == "a"):
+       add()
+    else:
+       subtract()
+
+def multiply():
+    Question.type = "numerical"
+    firstInt = random.randint(1,12)
+    secondInt = random.randint(1,12)
+    if(secondInt<0):
+      Question.question,Question.answer = (str(firstInt)+ " x" + " ("+str(secondInt)+")",firstInt*secondInt)
+    else:
+      Question.question,Question.answer = (str(firstInt)+" x "+str(secondInt),firstInt*secondInt)
+
+def divide():
+   Question.type = "numerical"
+   divisor = random.randint(3,12)
+   quotient = random.randint(3,12)
+   dividend = divisor * quotient
+   Question.question,Question.answer = (str(dividend)+" / "+str(divisor),quotient)
+
+def multiplyDivide():
+   Question.type = "numerical"
+   choice = random.choice('md')
+   if(choice=="m"):
+      multiply()
+   else:
+      divide()
+
+def allOperations():
+   Question.type = "numerical"
+   choice = random.choice('am')
+   if(choice == 'a'):
+      addSubtract()
+   else:
+      multiplyDivide()
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+#division problem
+#quotient is single digit no remainder
+#####
+#I can probably make all this division one function
+#####
+def divideSingleDigitQuotient():
+   qtype = "numerical"
+   divisor = random.randint(2,10)
+   quotient = random.randint(2,10)
+   dividend = divisor*quotient
+   Question.question = str(dividend)+"/"+str(divisor)
+   Question.answer = str(quotient)
+
+def divideSingleDigitQuotientMultiple10():
+   qtype = "numerical"
+   divisor = random.randint(2,10)
+   divisor *= 10
+   quotient = random.randint(2,10)
+   dividend = divisor*quotient
+   Question.question = str(dividend)+"/"+str(divisor)
+   Question.answer = str(quotient)
+
+def divideDoubleDigitQuotient():
+   qtype = "numerical"
+   divisor = random.randint(2,10)
+   quotient = random.randint(10,99)
+   dividend = divisor*quotient
+   Question.question = str(dividend)+"/"+str(divisor)
+   Question.answer = str(quotient)
+
+def divideDoubleDigitQuotientMultiple10():
+   qtype = "numerical"
+   divisor = random.randint(2,10)
+   divisor *= 10
+   quotient = random.randint(10,99)
+   dividend = divisor*quotient
+   Question.question = str(dividend)+"/"+str(divisor)
+   Question.answer = str(quotient)
+
+def divideDoubleDigitDivisor():
+   qtype = "numerical"
+   divisor = random.randint(11,50)
+   quotient = random.randint(10,99)
+   dividend = divisor*quotient
+   Question.question = str(dividend)+"/"+str(divisor)
+   Question.answer = str(quotient)
+
+def divideDecimalEasy():
+   qtype = "numerical"
+   divisor = random.randint(1,10)
+   divisor = divisor/10.0
+   quotient = random.randint(10,100)
+   dividend = divisor*quotient
+   Question.question = str(dividend)+"/"+str(divisor)
+   Question.answer = str(quotient)
+
+def divideDecimalMedium():
+   qtype = "numerical"
+   divisor = random.randint(1,10)
+   divisor = divisor/10.0
+   quotient = random.randint(100,1000)
+   dividend = divisor*quotient
+   Question.question = str(dividend)+"/"+str(divisor)
+   Question.answer = str(quotient)
+
+def divideDecimalHard():
+   qtype = "numerical"
+   divisor = random.randint(10,100)
+   divisor = divisor/10.0
+   quotient = random.randint(10,100)
+   dividend = divisor*quotient
+   Question.question = str(dividend)+"/"+str(divisor)
+   Question.answer = str(quotient)
 
 
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
 
-def addToSheetNumeric(function, Sheet):
-    for num in range(1,10):  #to iterate between first to second-1 number of questions
-        #function = add
-        Sheet.spreadsheet.write(Sheet.row,Sheet.column, 'Q' + str(num))
-        Sheet.spreadsheet.write(Sheet.row,Sheet.column+1,Question.question)
-        Sheet.row+=1
-        Sheet.spreadsheet.write(Sheet.row,Sheet.column,"Level")
-        Sheet.spreadsheet.write(Sheet.row,Sheet.column+1,Question.level)
-        Sheet.row+=1
-        Sheet.spreadsheet.write(Sheet.row,Sheet.column,"Question Type")
-        Sheet.spreadsheet.write(Sheet.row,Sheet.column+1,Question.answerType)
-        Sheet.row+=1
-        Sheet.spreadsheet.write(Sheet.row,Sheet.column,"Correct Answers")
-        #write an answer to a cell from function
-        Sheet.spreadsheet.write(Sheet.row,Sheet.column+1,Question.answer)
-        Sheet.row+=3;
-        print("Row in addToSheetNumeric = " +str(Sheet.row))
-
-def addToSheetMultipleChoice(function, Sheet):
-    for num in range(1,101):  #to iterate between first to second-1 number of questions
-        Sheet.ws.write(Question.row,Question.col, 'Q' + str(num))
-        #write a question from function
-        Sheet.ws.write(Question.row,Question.col+1,Question.question)
-        Question.row+=1
-        Sheet.ws.write(Question.row,Question.col,"Level")
-        Sheet.ws.write(Question.row,Question.col+1,Question.level)
-        Question.row+=1
-        Sheet.ws.write(Question.row,Question.col,"Question Type")
-        Question.answerType = "Multiple Choice"
-        Sheet.ws.write(Question.row,Question.col+1,Question.answerType)
-        Question.row+=1
-        Sheet.ws.write(Question.row,Question.col,"Correct Answers")
-        Sheet.ws.write(Question.row,Question.col+1,Question.answer)
-        #write an answer to a cell from function
-        for letter, choice in Question.multipleChoices.items():
-            Question.row+=1
-            Sheet.ws.write(Question.row,Question.col,letter)
-            Sheet.ws.write(Question.row,Question.col+1,choice)
-        Sheet.row+=3
+addition = [add, addMultipleChoice, subtract, addSubtract, multiply, divide, multiplyDivide, allOperations, divideSingleDigitQuotient]
 
 
-def runSheetMaker(functions):
-   w = Workbook()
-   ws = w.add_sheet('OK', cell_overwrite_ok=True)
-
-   Sheet.spreadsheet = ws
-
-   Sheet.row = 4
-   Sheet.column = 1
-   Question.level = 1
-   Question.answerType = 'Numeric'
-   ws.write(1,1,'Title')
-   ws.write(2,1,'Quiz')
-    #Determines if the answer type and sends the function to that for proper template
-   for f in functions:
-      f()
-      if(Question.answerType == "Numeric") or (Question.answerType == "AlphaNumeric"):
-          print("Adding Numeric")
-          addToSheetNumeric(functions, Sheet)
-          Question.level+=1
-      elif(Question.answerType == "MultipleChoice"):
-          addToSheetNumeric(functions, Sheet)
-          print("Adding multiple Choice")
-      else:
-          print("QuestionType is " + Question.answerType)
-   print(Sheet.row)
-   w.save('addition.xls')
-
-addition = [add, addMultipleChoice]
-
-runSheetMaker(addition)
+#def divideSingleDigitQuotientMultiple10():
+#def divideDoubleDigitQuotient():
+#def divideDoubleDigitQuotientMultiple10():
+#def divideDoubleDigitDivisor():
+#def divideDecimalEasy():
+#def divideDecimalMedium():
+#def divideDecimalHard():
 
 
-#questionType
-#name of spreadsheet
-#
-'''
-#working sheet creator
-for num in range(1,101):  #to iterate between first to second-1 number of questions
-   question, answer = modeOfNums()
-   question = str(question)
-   answer = str(answer)
-   ws.write(row,col, 'Q' + str(num))
-   #write a question from function
-   ws.write(row,col+1,question)
-   row+=1
-   ws.write(row,col,"Level")
-   ws.write(row,col+1,5)
-   row+=1
-   ws.write(row,col,"Question Type")
-   ws.write(row,col+1,"Numeric")
-   row+=1
-   ws.write(row,col,"Correct Answers")
-   #write an answer to a cell from function
-   ws.write(row,col+1,answer)
-   row+=3
-   '''
+Sheet.initialSheet()
+Sheet.insertProblem(add)
+Sheet.insertProblem(subtract)
+Sheet.insertProblem(addMultipleChoice)
+Sheet.insertProblem(addSubtract)
+Sheet.insertProblem(multiply)
+Sheet.insertProblem(divide)
+Sheet.insertProblem(multiplyDivide)
+Sheet.insertProblem(allOperations)
+Sheet.insertProblem(divideSingleDigitQuotient)
 
-'''
 
-for num in range(1,101):  #to iterate between first to second-1 number of questions
-   question, answer = percentageIntro()
-   question = str(question)
-   answer = str(answer)
-   ws.write(row,col, 'Q' + str(num))
-   #write a question from function
-   ws.write(row,col+1,question)
-   row+=1
-   ws.write(row,col,"Level")
-   ws.write(row,col+1,1)
-   row+=1
-   ws.write(row,col,"Question Type")
-   Question.answerType = "Multiple Choice"
-   ws.write(row,col+1,Question.answerType)
-   row+=1
-   ws.write(row,col,"Correct Answers")
-   #write an answer to a cell from function
-   if(answer == 'part'):
-      ws.write(row,col+1,"1:A")
-   elif(answer == 'whole'):
-      ws.write(row,col+1,"1:B")
-   elif(answer == 'percent'):
-      ws.write(row,col+1,"1:C")
-   row+=1
-   ws.write(row,col,"A")
-   ws.write(row,col+1,"Part")
-   row+=1
-   ws.write(row,col,"B")
-   ws.write(row,col+1,"Whole")
-   row+=1
-   ws.write(row,col,"C")
-   ws.write(row,col+1,"Percent")
-   row+=3
-'''
-#lastRow = addToSheet(0)
-#w.save('mode2.xls')
+Sheet.w.save("addition.xls")
+
 print("Awesome")
